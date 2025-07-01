c4a1375bd0632f14bebc439119d8ea13b095ae32
FTMustafa <mustafamustafatw@gmail.com>
2025-07-01 19:24:00 +0300

    Skills page and cursors added

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,20 +1,67 @@
 import "./Skills.css";
 import { useEffect } from "react";
+import {
+  FaReact,
+  FaNodeJs,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaGitAlt,
+  FaNpm,
+} from "react-icons/fa";
+import { SiTailwindcss, SiExpress, SiMysql } from "react-icons/si";
+import ScrambleText from "../../components/ScrambleText";
 
 function Skills() {
   useEffect(() => {
     document.body.style.overflowY = "hidden";
-
     const timer = setTimeout(() => {
       document.body.style.overflowY = "auto";
     }, 500);
-
     return () => {
       document.body.style.overflowY = "auto";
       clearTimeout(timer);
     };
   }, []);
-  return <div className="skills fade-in">Skills Page</div>;
+
+  const skills = [
+    { icon: <FaHtml5 />, name: "HTML" },
+    { icon: <FaCss3Alt />, name: "CSS" },
+    { icon: <FaJs />, name: "JavaScript" },
+    { icon: <FaReact />, name: "React" },
+    { icon: <SiTailwindcss />, name: "Tailwind" },
+    { icon: <FaNodeJs />, name: "Node.js" },
+    { icon: <SiExpress />, name: "Express.js" },
+    { icon: <SiMysql />, name: "MySQL" },
+    { icon: <FaGitAlt />, name: "Git" },
+    { icon: <FaNpm />, name: "NPM" },
+  ];
+
+  return (
+    <div className="skills fade-in">
+      <h2 className="skills-title">
+        <ScrambleText text={"🛠 My Skills"} duration={1000} />
+      </h2>
+      <div className="honeycomb">
+        {skills.map((skill, i) => (
+          <div className="hex-svg-wrapper" key={i}>
+            <svg viewBox="0 0 100 100" className="hex-svg">
+              <polygon
+                points="50,0 93,25 93,75 50,100 7,75 7,25"
+                className="hex-shape"
+              />
+              <foreignObject x="20" y="5" width="60" height="100">
+                <div className="hex-content" xmlns="http://www.w3.org/1999/xhtml">
+                  <div className="icon">{skill.icon}</div>
+                  <p>{skill.name}</p>
+                </div>
+              </foreignObject>
+            </svg>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
